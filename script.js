069e01f82eb942558afb18f0df4ef2ab132c7e87
inami-estage-being <poweradefuelx@yahoo.co.jp>
2026-07-04 23:23:36 +0900

    テスト版をリリース

---
+++ script.js
@@ -0,0 +1,19 @@
+function clickClock() {
+    alert("時計を調べた。");
+  }
+  
+  function clickPC() {
+    alert("PCを調べた。");
+  }
+  
+  function clickBox() {
+    alert("箱を調べた。");
+  }
+
+  function clickDoor() {
+    alert("ドアを調べた。");
+  }
+
+  function clickSyodana() {
+    alert("書棚を調べた。");
+  }
